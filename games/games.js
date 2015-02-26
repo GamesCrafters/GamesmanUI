5aaf0a07fab74b56b5361d48db9fec7e38d18a00
lespaul <kevinlee324@gmail.com>
2015-02-25 16:05:12 -0800

    added working menu skeleton; see comment in webui.js

---
+++ games/games.js
@@ -0,0 +1,45 @@
+/* this file contains json describing each game for the menu's sake */
+var games = {
+  "ttt": {
+    "img": "games/ttt/ttt.png",
+    "src": "games/ttt/ttt.js",
+    "title": "Tic Tac Toe",
+    "desc": "The classic game of X's and O's"
+  },
+  "stt": {
+    "img": "games/stt/stt.png",
+    "src": "games/stt/stt.js",
+    "title": "Shift Tac Toe",
+    "desc": "Tic Tac Toe with a twist"
+  },
+  "stt2": {
+    "img": "games/stt/stt.png",
+    "src": "games/stt/stt.js",
+    "title": "Shift Tac Toe",
+    "desc": "Tic Tac Toe with a twist"
+  },
+  "stt3": {
+    "img": "games/stt/stt.png",
+    "src": "games/stt/stt.js",
+    "title": "Shift Tac Toe",
+    "desc": "Tic Tac Toe with a twist"
+  },
+  "stt4": {
+    "img": "games/stt/stt.png",
+    "src": "games/stt/stt.js",
+    "title": "Shift Tac Toe",
+    "desc": "Tic Tac Toe with a twist"
+  },
+  "stt5": {
+    "img": "games/stt/stt.png",
+    "src": "games/stt/stt.js",
+    "title": "Shift Tac Toe",
+    "desc": "Tic Tac Toe with a twist"
+  },
+  "stt6": {
+    "img": "games/stt/stt.png",
+    "src": "games/stt/stt.js",
+    "title": "Shift Tac Toe",
+    "desc": "Tic Tac Toe with a twist"
+  }
+}
--- webui.js
+++ webui.js
@@ -1,3 +1,31 @@
+$(document).ready( function() {
+  // iterate through games/games.json
+  // which we assume to contain information about games in games/<gamename>
+  // append a menu item to #menu for each game, embed the right <gamename>.js
+  // file in the data-game-src attribute
+  //
+  // this <gamename>.js file should attach handlers, render, etc
+  // to the #gameBoard div element
+  for (var key in games) {
+    if (games.hasOwnProperty(key)) {
+      var game = "";
+      game += "<div class='game flex-elem' id='" + key + "' ";
+      game +=       "data-game-src='" + games[key].src + "'>\n";
+      game += "  <a href='#'><img src='" + games[key].img + "'></a>\n";
+      game += "  <h3 class='game-title'>" + games[key].title + "</h3>\n";
+      game += "  <p class='game-desc'>" + games[key].desc + "</p>\n";
+      game += "</div>";
+      $("#menu").append(game);
+    }
+  }
+
+  $(".game a").on("click", function() {
+    $("#gameScreen").show();
+    $("#menu").hide();
+    $.getScript($(this).parent().attr("data-game-src"));
+  });
+});
+
 $(function () {
   refresh();
   $('#game-input').on('input', refresh);
