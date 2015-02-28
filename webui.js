4b781aa007d8299c4d561c61a87594b616c863df
Vigenere36 <Vigenere@airbears2-10-142-31-127.airbears2.1918.berkeley.edu>
2015-02-27 16:02:35 -0800

    learning how to parse json

--- webui.js
+++ webui.js
@@ -15,29 +15,52 @@ function refresh() {
 }
 */
 
-var s = Snap("#main-svg");
-var epsilon = 5;
-var svgWidth = parseInt($("#main-svg").attr("width")) - epsilon;
-var svgHeight = parseInt($("#main-svg").attr("height")) - epsilon;
-
-var i;
-var boardLines = [];
-for (i = 0; i <= 3; i++) {
-  var varWidth = (svgWidth * i)/3;
-  var varHeight = (svgWidth * i)/3;
-  var hozLine = s.line(0, varHeight, svgWidth, varHeight);
-  var vertLine = s.line(varWidth, 0, varWidth, svgHeight);
-  hozLine.attr({"stroke":"black", "stroke-width": 1});
-  vertLine.attr({"stroke":"black", "stroke-width": 1});
-}
+//Server request functions
 
 function getStart(game, callback) {
-  $.get('http://nyc.cs.berkeley.edu:8081/' + game + '/getStart', function (res) {
+  //$.get('http://nyc.cs.berkeley.edu:8081/' + game + '/getStart', function (res) {
+  $.get('http://localhost:8081/' + game + '/getStart', function (res) {
     var res = JSON.parse(res);
     callback(res.response);
   });
 }
 
+function tttParse(response) {
+  console.log(response);
+}
+
+//Board Functions
+
+function initBoard(game) {
+  if (game == "ttt") {
+    var i;
+    var boardLines = [];
+    for (i = 0; i <= 3; i++) {
+      var varWidth = (svgWidth * i)/3;
+      var varHeight = (svgWidth * i)/3;
+      var hozLine = s.line(0, varHeight, svgWidth, varHeight);
+      var vertLine = s.line(varWidth, 0, varWidth, svgHeight);
+      hozLine.attr({"stroke":"black", "stroke-width": 1});
+      vertLine.attr({"stroke":"black", "stroke-width": 1});
+    }
+  }
+}
+
 function drawBoard (svg, boardString) {
   console.log('drawing', boardString);
 }
+
+//Initialize game
+var path = location.pathname.split("/");
+var game = path[path.length - 1].split(".")[0];
+
+var s = Snap("#main-svg");
+var epsilon = 5;
+var svgWidth = parseInt($("#main-svg").attr("width")) - epsilon;
+var svgHeight = parseInt($("#main-svg").attr("height")) - epsilon;
+
+//Set up board for game
+initBoard(game)
+
+//Request starting board information from server
+getStart(game, tttParse);
