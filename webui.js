63a45e504e0cb83f459e81748362eba8a59d05ba
Vigenere36 <Vigenere@airbears2-10-142-30-182.airbears2.1918.berkeley.edu>
2015-03-02 18:20:56 -0800

    Got initial getStart working. (Run the server locally)

--- webui.js
+++ webui.js
@@ -1,38 +1,31 @@
-/*
-$(function () {
-  refresh();
-  $('#game-input').on('input', refresh);
-  $('#position').on('input', function () {
-    drawBoard(Snap('#main-svg'), $('#position').val());
-  });
-});
-
-function refresh() {
-  getStart($('#game-input').val(), function (start) {
-    $('#position').val(start);
-    drawBoard(Snap('#main-svg'), start);
-  });
-}
-*/
-
 //Server request functions
 
-function getStart(game, callback) {
-  //$.get('http://nyc.cs.berkeley.edu:8081/' + game + '/getStart', function (res) {
-  $.get('http://localhost:8081/' + game + '/getStart', function (res) {
+function getStart(callback) {
+  $.get(HOST + GAME + '/getStart', function (res) {
     var res = JSON.parse(res);
     callback(res.response);
   });
 }
 
-function tttParse(response) {
-  console.log(response);
+function getNextMoves(callback, board) {
+  $.get(HOST + GAME + '/getNextMoveValues?board="' + board + '"', function(res) {
+    var res = JSON.parse(res);
+    callback(res.response);
+  });
+}
+
+function getCallback() {
+  if (GAME == "ttt") {
+    return tttRun;
+  } else {
+    return None;
+  }
 }
 
 //Board Functions
 
-function initBoard(game) {
-  if (game == "ttt") {
+function initBoard() {
+  if (GAME == "ttt") {
     var i;
     var boardLines = [];
     for (i = 0; i <= 3; i++) {
@@ -46,21 +39,69 @@ function initBoard(game) {
   }
 }
 
-function drawBoard (svg, boardString) {
-  console.log('drawing', boardString);
+//Game Specific Functions
+
+//Tic-Tac-Toe
+function tttRun(response) {
+  getNextMoves(tttDraw, response);
 }
 
+function tttDraw(response) {
+  //Draw possible moves
+  for (var i = 0; i < response.length; i++) {
+    move = response[i].move - 1; //We want it to be 0-indexed
+    var posX = (parseInt(move) % 3) * (svgWidth/3) + (svgWidth/6);
+    var posY = Math.floor(parseInt(move) / 3) * (svgHeight/3) + (svgHeight/6);
+
+    var rectWidth = 15, rectHeight = 150;
+    var rect1 = s.rect(0, 0, rectWidth, rectHeight);
+    var rect2 = s.rect(0, 0, rectWidth, rectHeight);
+
+    var factor = Math.cos(Math.PI/4)/2;
+    var rect1X = (posX + rectHeight * factor - rectWidth * factor).toString();
+    var rect2X = (posX - rectHeight * factor - rectWidth * factor).toString();
+    var rect1Y = (posY - rectHeight * factor - rectWidth * factor).toString();
+    var rect2Y = (posY - rectHeight * factor + rectWidth * factor).toString();
+
+    //Purpose of circle is to let hover be activated when cursor is near the X, but not exactly on
+    var circle = s.circle(posX, posY, Math.min(svgWidth, svgHeight)/6 - 5);
+    var x = s.group(circle, rect1, rect2);
+
+    x.attr({
+      fill: "#eef"
+    });
+    circle.attr({
+      opacity: 0
+    });
+    rect1.attr({
+      transform: 'translate(' + rect1X + ',' + rect1Y + ') rotate(45, 0, 0)'
+    });
+    rect2.attr({
+      transform: 'translate(' + rect2X + ',' + rect2Y + ') rotate(-45, 0, 0)'
+    });
+  }
+}
+
+
+/*
+Important Variables:
+
+HOST - server to query
+GAME - the name of game being played
+s - the svg to draw on
+svgWidth, svgHeight - dimensions of svg
+*/
+
 //Initialize game
+var HOST = 'http://localhost:8081/';
 var path = location.pathname.split("/");
-var game = path[path.length - 1].split(".")[0];
+var GAME = path[path.length - 1].split(".")[0];
 
 var s = Snap("#main-svg");
 var epsilon = 5;
 var svgWidth = parseInt($("#main-svg").attr("width")) - epsilon;
 var svgHeight = parseInt($("#main-svg").attr("height")) - epsilon;
 
-//Set up board for game
-initBoard(game)
-
-//Request starting board information from server
-getStart(game, tttParse);
+//Draw initial board
+initBoard()
+getStart(getCallback());
